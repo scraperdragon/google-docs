dd6eeecdbe5bf28b5f6de15be379c558867cce25
Dragon Dave McKee <dragon@scraperwiki.com>
2015-04-10 15:10:52 +0000

    nicer output, redirect

--- cgi-bin/savetoken.py
+++ cgi-bin/savetoken.py
@@ -5,18 +5,24 @@ import requests
 import json
 import urlparse
 
-def output(msg):
-    print json.dumps(msg)
-    exit(0)
-
-print "Content-type: application/json\n\n"; 
+print "Content-type: text/html\n\n"; 
 
 params = urlparse.parse_qs(os.environ.get("QUERY_STRING"))
 key, = params.get('key')
 
 if not(key):
-    output({"error": "no key"})
+    print "<html><body><h1>No key</h1></body></html>"
+    exit(1)
 else:
     with open("/home/google_api_key.json", "w") as f:
         json.dump(key, f)
-    output({"key": key})
+    print """ <html>    
+  <head>      
+    <title>Saved key: redirecting</title>      
+    <meta http-equiv="refresh" content="0;URL='fullprocess'" />    
+  </head>    
+  <body> 
+    <p>saved key: processing... should be refreshing, otherwise try <a href='fullprocess'>fullprocess</a></p> 
+  </body>  
+</html>     """
+    exit(0)
